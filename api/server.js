dbd8c19b30771d8cfbee51d479caadf3d7449011
naushinhoque <fnmh@yahoo.com>
2023-11-20 17:47:47 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -1,11 +1,12 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require('express')
-
+const postRouter = require('./posts/posts-router')
 const server = express()
 
 server.use(express.json())
 
+server.use('/api/posts', postsRouter)
 
 server.use('*', (req, res) => {
     res.status(404).json({
